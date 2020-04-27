45fb0da15001ec72c1ba4cbc1adfd7fbed07752a
Ra <rrequero@gmail.com>
2020-04-27 15:52:56 +0200

    fix connect to cloudsql

--- src/tile-server/routers/mvt.router.ts
+++ src/tile-server/routers/mvt.router.ts
@@ -26,7 +26,7 @@ function getPoolByDataset(dataset) {
     if (process.env.NODE_ENV === 'dev') {
       connection.host = 'localhost';
     } else if (dataset.target.type === 'cloudsql') {
-      connection.host = `/cloudsql/${dataset.target.database.projectId}-${dataset.target.database.region}-${dataset.target.database.instanceId}`;
+      connection.host = `/cloudsql/${dataset.target.database.projectId}:${dataset.target.database.region}:${dataset.target.database.instanceId}`;
     } else {
       connection.host = dataset.target.database.host;
     }
