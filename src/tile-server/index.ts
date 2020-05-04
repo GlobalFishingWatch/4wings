0ed97869e127b6f396624b6dd5e70df412b2b855
Ra <rrequero@gmail.com>
2020-05-04 17:52:27 +0200

    configure timeout to 900s

--- src/tile-server/index.ts
+++ src/tile-server/index.ts
@@ -39,7 +39,8 @@ export async function start(args) {
 
   app.use(mvtRouter.routes()).use(mvtRouter.allowedMethods());
 
-  app.listen(process.env.PORT || 3000, () => {
+  const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Running mvt tile server in port ${process.env.PORT || 3000}`);
   });
+  server.setTimeout(900000);
 }
