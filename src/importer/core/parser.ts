624954766d7347054c8f557a580f215ff95a3e9c
Ra <rrequero@gmail.com>
2021-03-12 09:59:25 +0100

    improve parser

--- src/importer/core/parser.ts
+++ src/importer/core/parser.ts
@@ -95,23 +95,13 @@ export function parseElement(options: any, origData: any): any[] {
     data.htime = Math.floor(dateTrunc.getTime() / time);
     data.timestamp = new Date(data.timestamp / 1000).toISOString();
   }
-
-  const nums = getTileNums(data.lon, data.lat, options.maxZoom);
-
-  const cells = getCells(
-    options.maxZoom,
-    options.cellsByZoom,
-    data.lon,
-    data.lat,
-  );
-
   const result = [];
   for (let i = 0; i <= options.maxZoom; i++) {
     const el: any = {
       lat: data.lat,
       lon: data.lon,
-      pos: nums[i],
-      cell: cells[i],
+      pos: origData.positions[i],
+      cell: origData.cells[i],
       timestamp: data.timestamp,
       zoom: i,
     };
@@ -121,9 +111,37 @@ export function parseElement(options: any, origData: any): any[] {
     options.extraColumns.forEach((k) => {
       el[k] = data[k];
     });
-
     result.push(el);
   }
+
+  // const nums = getTileNums(data.lon, data.lat, options.maxZoom);
+
+  // const cells = getCells(
+  //   options.maxZoom,
+  //   options.cellsByZoom,
+  //   data.lon,
+  //   data.lat,
+  // );
+
+  // const result = [];
+  // for (let i = 0; i <= options.maxZoom; i++) {
+  //   const el: any = {
+  //     lat: data.lat,
+  //     lon: data.lon,
+  //     pos: nums[i],
+  //     cell: cells[i],
+  //     timestamp: data.timestamp,
+  //     zoom: i,
+  //   };
+  //   if (options.heatmap) {
+  //     el.htime = data.htime;
+  //   }
+  //   options.extraColumns.forEach((k) => {
+  //     el[k] = data[k];
+  //   });
+
+  //   result.push(el);
+  // }
 
   return result;
 }
